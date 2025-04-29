1b72e71ccef52a82ede136e1ebe705518d20c02b
Adelia Jupiter <cjadelia@gmail.com>
2025-04-29 18:38:51 -0400

    did exercises 1-5

--- app.js
+++ app.js
@@ -4,7 +4,41 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Adelia";
+let lastName;
+var age;
 
 // Exercise 2
+//firstName assigned in Exercise 1
+lastName = "Jupiter";
+age = 30;
+
+console.log(`My name is ${firstName} ${lastName} and I am ${age}-years-old`);
 
 // Exercise 3
+let language = "Javascript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+console.log(`${language} was created in ${createdYear}`);
+
+//Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+console.log(
+  `This item in size ${selectedSize} is $${19.99} and
+  there are ${stock} in stock.`);
+
+//Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+console.log(`Reading: ${title} by ${author}, page ${bookmark}/${pageCount}`);
